6bad53d8f4c7b55c8d2875bdb17718fd4530b166
Dashdorj <ddash.g168@gmail.com>
2022-09-08 09:32:58 +0800

    gitpage ashiglahiin tuld docs folder uusgelee

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-const arr = [88, 45, 54, 63, 56, 4];
-let myfuns = a => console.log(`too : ${a}`)
-const arr2 = [...arr, 44,55,2]
-myfuns(arr2[1]);
+require("@babel/polyfill");
+import Search from "./model/Search";
+
+let search = new Search("pasta");
+search.doSearch().then(r => console.log(r))
